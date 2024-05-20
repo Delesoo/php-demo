7baf16dc7dd67d114ee925dcd84fee4a56265270
Delesoo <strongtoka@gmail.com>
2024-05-20 20:50:29 +0400

    crash demo

--- demo/index.php
+++ demo/index.php
@@ -1,5 +1,19 @@
 <?php
 
 require 'functions.php';
+// require 'router.php';
 
-require 'router.php';
+//connect to mySQL database
+$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4';
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare('select * from posts');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
+
